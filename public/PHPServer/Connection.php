1543a6ec798277ac8edcdc92fd6e731c903c6717
maxzirilli <massimiliano.zirilli@gmail.com>
2026-07-21 15:35:36 +0200

    Connection

--- public/PHPServer/Connection.php
+++ public/PHPServer/Connection.php
@@ -6,12 +6,6 @@
       header("Content-Type: application/json;charset=UTF-8");
       header(ACCESS_CONTROLL_SHARED);
       header("Access-Control-Allow-Methods:GET,POST,OPTIONS");
-      header("Access-Control-Allow-Headers: ParSessionId, Content-Type");
-
-      if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(200);
-            exit();
-      }
 
       $AConnection = new TAdvQuery();
       switch($_SERVER['REQUEST_METHOD'])
